ec1f84c371916f00fcfd72ed3edd4016666b2d9d
Bùi Hiển <95581165+RealaxER@users.noreply.github.com>
2024-04-26 16:08:37 +0700

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -7,11 +7,11 @@ pub mod logic;
 pub mod sql;
 pub mod system_intergration;
 pub mod transport;
-use env_logger::Builder;
-use std::fs::File;
-use log::LevelFilter;
-use chrono::Local;
-use std::io::Write;
+// use env_logger::Builder;
+// use std::fs::File;
+// use log::LevelFilter;
+// use chrono::Local;
+// use std::io::Write;
 
 
 
@@ -19,37 +19,19 @@ use std::io::Write;
 #[command(author, version, about, long_about = None)]
 struct Args {
     #[clap(short, long)]
-    log: String,
-    
-    #[clap(short, long)]
     db: String,
 }
 
 
 #[tokio::main]
 async fn main() {
+
+    env_logger::builder().format_timestamp_millis().init();
+
     let args = Args::parse();
     log::info!("args: {:?}", args);
 
-    let target = Box::new(File::create(args.log).expect("Can't create file"));
-    Builder::new()
-        .format(|buf, record| {
-            writeln!(
-                buf,
-                "{}:{} {} [{}] - {}",
-                record.file().unwrap_or("unknown"),
-                record.line().unwrap_or(0),
-                Local::now().format("%Y-%m-%dT%H:%M:%S%.3f"),
-                record.level(),
-                record.args()
-            )
-        })
-        .target(env_logger::Target::Pipe(target))
-        .filter(None, LevelFilter::Info)
-        .init();
-
     let mut system_intergration = SystemIntergration::new(args.db).await;
-    log::info!("Bui Dinh Hien");
     system_intergration.init().await;
 
     loop {
